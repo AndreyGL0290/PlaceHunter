fe38f5fcaf27d1a575f5b02b9fd54d9a9347edff
Andrew Glukhov <andriy.glukhov@yandex.rucd>
2021-08-29 22:19:20 +0700

    Cut out auth code and placed it to router/auth file

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 // Import
-const dotenv = require('dotenv');
-const bcrypt = require('bcrypt');
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const nodemailer = require('nodemailer');
-const sign = require('jwt-encode');
-const mysql = require('mysql2');
 const { checkNoTokenCookie, checkTokenCookie, checkTokenDBToURL } = require('./middlewares/checktoken');
-const sportRouter = require('./sportRouter');
+const cookieParser = require('cookie-parser');
+const sport = require('./routes/sport');
+const auth = require('./routes/auth');
+const express = require('express');
+const dotenv = require('dotenv');
+const mysql = require('mysql2');
 
 // Reads .env file
 dotenv.config()
@@ -16,10 +13,7 @@ dotenv.config()
 // Init variables
 const app = express();
 const jsonParser = express.json();
-const saltRounds = 10;
 const port = 8080;
-let password;
-let email;
 
 // Mysql server configuration
 const con = mysql.createConnection({
@@ -34,46 +28,28 @@ con.connect(function (err) {
      if (err) throw err;
 });
 
-// Nodemailer configuration
-const transporter = nodemailer.createTransport({
-     service: 'gmail',
-     auth: {
-          user: process.env.USER,
-          pass: process.env.PASSWORD
-     }
-});
-
 // Static Files
 app.use(express.static('public'));
 app.use('/css', express.static(__dirname + 'public/css'));
 app.use('/img', express.static(__dirname + 'public/img'));
 app.use('/js', express.static(__dirname + 'public/js'));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/sport', sportRouter);
+app.use('/sport', sport);
+app.use('/authorization', auth);
 app.use(cookieParser());
 
 // Set views
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
-// basic instance that helps to 
-
 // Send information from server to client
 // When we come to the site we render home.ejs file
 app.get('', (req, res) => {
+     res.cookie('test', 123, { maxAge: 60000 * 5, path: 'http://localhost:8080/', httpOnly: true });
      res.render('home');
 })
 
 app.get('/account', checkTokenCookie, (req, res) => {
      res.render('account');
-})
-
-app.get('/registration', checkNoTokenCookie, (req, res) => {
-     res.render('registration');
-})
-
-app.get('/login', checkNoTokenCookie, (req, res) => {
-     res.render('login');
 })
 
 app.get('/confirm', checkNoTokenCookie, checkTokenDBToURL, (token, req, res, next) => {
@@ -97,90 +73,6 @@ app.post('', jsonParser, (req, res) => {
                }
           } else {
                res.json('Сперва вам нужно войти в свой аккаунт');
-          }
-     });
-})
-
-// Sign up page
-app.post('/registration', jsonParser, (req, res) => {
-     if (!req.body) return res.sendStatus(400);
-     email = req.body.email;
-     password = req.body.password1;
-     if (email != '') {
-          if (password || false) {
-               if (req.body.password1 == req.body.password2) {
-                    // Pushes heshed password and email to BD
-                    bcrypt.genSalt(saltRounds, function (err, salt) {
-                         bcrypt.hash(password, salt, function (err, hash) {
-                              if (err) throw err;
-                              con.query(`SELECT email token FROM accounts WHERE email='${email}'`, function (err, result) {
-                                   if (err) throw err;
-
-                                   if (result[0] == undefined) {
-                                        const jwt = sign({ email: email }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES });
-                                        con.query(`INSERT INTO accounts (email, password, token) VALUES ('${email}', '${hash}', '${jwt}')`, function (err, result) {
-                                             if (err) throw err;
-                                             con.query(`INSERT INTO add_info (token) VALUES ('${jwt}')`, (err, result) => {
-                                                  if (err) throw err;
-
-                                             })
-                                             res.json('')
-                                             const mailOptions = {
-                                                  from: process.env.USER,
-                                                  to: email,
-                                                  subject: 'Sportbook regestration',
-                                                  html: `<style>.button {display:inline-block;position:relative;color:black;background-color:rgba(0, 0, 0, 0);text-decoration:none;text-transform:uppercase;border:2px solid black;transition:250ms;width:280px;height:40px;line-height:40px;text-align:center;}</style>
-                                                  <p>Ваша электронная почта была использована для регестрации на сайте sportbook.com.<br>
-                                                       Если это не вы пытаетесь зарегестрироваться на нашем сайте, то проигнорируте это письмо<br>
-                                                       Если это вы хотите зарегестрироваться на нашем сайте, то нажмите кнопку "Окончить регестрацию"</p>
-                                                       <a class='button' href='http://localhost:8080/confirm?token=${jwt}'>Окончить регестрацию</a>`,
-                                             }
-                                             transporter.sendMail(mailOptions, function (error, info) {
-                                                  if (error) {
-                                                       console.log(error);
-                                                  } else {
-                                                       console.log('Email sent: ' + info.response);
-                                                  }
-                                             });
-                                        })
-
-                                   } else {
-                                        if (result[0].token === undefined || result[0].token === null) res.json('Вам уже было отправлено письмо');
-                                        else { res.json('К этой почте уже привязан аккаунт') }
-                                   }
-                              });
-                         });
-                    });
-               } else {
-                    res.json('Пароли не совпадают');
-               }
-          } else {
-               res.json('Введите пароль');
-          }
-     } else {
-          res.json('Введите почту');
-     }
-});
-
-// Log in page
-app.post('/login', jsonParser, function (req, res) {
-     if (!req.body) return res.sendStatus(400);
-     email = req.body.email;
-     password = req.body.password1;
-     con.query(`SELECT email, password, token FROM accounts WHERE email='${email}'`, function (err, result) {
-          if (err) throw err;
-          if (result[0] !== undefined && result[0] !== null) {
-               bcrypt.compare(password, result[0].password, function (err, result1) {
-                    if (err) throw err;
-                    if (result1) {
-                         res.cookie('access_token', result[0].token, { maxAge: 3600000 * 8, httpOnly: true });
-                         res.json('');
-                    } else {
-                         res.json('Неправилная почта или пароль');
-                    }
-               });
-          } else {
-               res.json('Неправилная почта или пароль');
           }
      });
 })
