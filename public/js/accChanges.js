93997c209370ff55010912ff8b19e29544859bf7
Andrew Glukhov <andriy.glukhov@yandex.rucd>
2021-09-06 23:06:01 +0700

    Made little custom validator

--- public/js/accChanges.js
+++ public/js/accChanges.js
@@ -2,8 +2,11 @@ let formName = document.getElementsByClassName('add-info')[0].getAttribute('name
 document.getElementsByClassName("add-info-button")[0].addEventListener("click", function (e) {
     e.preventDefault();
     let name = document.forms[formName].elements['name'].value;
+    if (name === '') name = 'Undefined';
     let avatar = document.forms[formName].elements['avatar'].value;
+    if (avatar === '') avatar = 'Undefined';
     let age = document.forms[formName].elements['age'].value;
+    if (age === '') age = 'Undefined';
     // Сериализуем данные в JSON
     let data = JSON.stringify({ name: name, avatar: avatar, age: age });
     let request = new XMLHttpRequest;
