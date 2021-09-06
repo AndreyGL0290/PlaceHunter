a7cb9012f059e627bffeeac83c393afaa5d96a91
Andrew Glukhov <andriy.glukhov@yandex.rucd>
2021-09-06 23:08:36 +0700

    Added getting info to the client side and react render tools

--- public/js/search.js
+++ public/js/search.js
@@ -2,33 +2,66 @@
 const headerHeight = document.getElementById('header').clientHeight + 'px';
 document.getElementById('content-container').style.marginTop = headerHeight;
 
-// Проверяем есть ли у браузера пользователя передовать его местоположение
+// Проверяем есть ли у браузера пользователя возможность передовать его местоположение
 let geo = false;
 if (navigator.geolocation) geo = true;
-let receivedData;
 
 // Сначала запросим информацию с сервера
-const request = new XMLHttpRequest();
-request.open('POST', '/', true);
+const request = new XMLHttpRequest;
+request.open('POST', '', true);
 request.setRequestHeader("Content-Type", "application/json");
 request.addEventListener("load", function () {
-    receivedData = JSON.parse(request.response);
-    console.log(receivedData);
+    let recievedData = JSON.parse(request.response);
+
+    // Данные аккаунта
+    const avatar = recievedData.avatar1;
+    const userName = recievedData.name1;
+    const age = recievedData.age1;
+    const sport = recievedData.sport1;
+    const level = recievedData.level1;
 
     const app = (
-        <Usercard />
+        <Usercard avatar={avatar} userName={userName} age={age} sport={sport} level={level}/>
     )
 
-    ReactDOM.render(app, document.getElementById('variants'));
+    if (!recievedData.sport1) {
+        document.getElementsByClassName("filter-error")[0].textContent = recievedData.error;
+        document.getElementsByClassName("card-input")[0].value = recievedData.sport;
+        document.getElementsByClassName("card-input")[1].value = recievedData.level;
+        document.getElementsByClassName('card')[0].style.backgroundColor = '#90ee90';
+    } else if (recievedData.sportType === undefined) {
+        document.getElementsByClassName("card-input")[0].value = recievedData.sport;
+        document.getElementsByClassName("card-input")[1].value = recievedData.level;
+        document.getElementsByClassName('card')[0].style.backgroundColor = '#90ee90';
+        ReactDOM.render(app, document.getElementById('variants'));
+    } else {
+        document.getElementsByClassName('card-input')[0].value = recievedData.sportType;
+        document.getElementsByClassName('card-input')[1].value = 'Любой';
+
+        document.getElementsByClassName('card')[0].style.backgroundColor = '#90ee90';
+        const request = new XMLHttpRequest;
+        request.open('POST', '', true);
+        request.setRequestHeader("Content-Type", "application/json");
+        request.send(JSON.stringify({ sport: sportInp, level: levelInp }));
+
+        ReactDOM.render(app, document.getElementById('variants'));
+    }
 })
 
 function Usercard(props) {
     return (
-        <div className='user-card'>
-            <div className='card-image'></div>
-            <div className='card-info'></div>
+        <div className="user-card">
+            <div className="card-image">
+                <img className="search-avatar" src={props.avatar} alt='Аватар'/>
+            </div>
+            <div className="card-info">
+                <p className="user-name">Имя: {props.userName}</p>
+                <p className="user-age">Возраст: {props.age}</p>
+                <p className="user-sport">Игра: {props.sport}</p>
+                <p className="user-level">Уровень: {props.level}</p>
+            </div>
         </div>
     )
 }
 
-request.send();
+request.send(JSON.stringify({ startSettings: true }));
