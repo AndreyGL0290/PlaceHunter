33402c7b082b0d22695b46a24ab6784b9fdd1c4a
AndreyGL0290 <andreygl2014@gmail.com>
2023-06-22 12:10:24 -0400

    Deleted unnecessary files

--- public/js/home.js
+++
@@ -1,3 +0,0 @@
-// Исправление отступа от header
-const headerHeight = document.getElementById('header').clientHeight + 'px';
-document.getElementById('content-container').style.marginTop = headerHeight;
