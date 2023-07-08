a9fd72c67bb1d28b799d4e98bcf732f0849cb8dc
AndreyGL0290 <andreygl2014@gmail.com>
2023-07-08 13:03:00 -0400

    Added a few divs so that content of the page was properly centered and streched between header and footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import '/public/css/globals.css'
 import { UserProvider } from '@auth0/nextjs-auth0/client';
-import NavBar from '../components/NavBar'
-import Footer from '../components/Footer'
+import '/public/css/globals.css';
+import React from 'react';
+
+import NavBar from '../components/NavBar';
+import Footer from '../components/Footer';
 
 export default function App({ Component, pageProps }) {
   return (
       <UserProvider>
-        <NavBar />
-        <Component {...pageProps} />
-        <Footer />
+        <div className='flex flex-col h-screen'>
+          <NavBar />
+          <div className='flex-grow'>
+            <Component {...pageProps} />
+          </div>
+          <Footer />
+        </div>
       </UserProvider>
   );
 }
