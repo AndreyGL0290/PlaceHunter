dec276e567a973a4f5febc7348b3bce6d92b5b2b
AndreyGL0290 <andreygl2014@gmail.com>
2023-07-08 12:44:14 -0400

    Added div tag inside of footer tag

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,10 @@
 export default function Footer() {
     return (
-        <footer id="footer" className="flex justify-evenly items-center h-28 mt-32 bg-black text-white">
-            <p className="">Email: andreygl2014@gmail.com</p>
-            <p className="">Phone: +1 (904) 789 94 88<br />Andrey Glukhov</p>
+        <footer id="footer">
+            <div className="h-28 flex flex-row justify-evenly items-center bg-black text-white">
+                <p className="">Email: andreygl2014@gmail.com</p>
+                <p className="">Phone: +1 (904) 789 94 88<br />Andrey Glukhov</p>
+            </div>
         </footer>
     )
 }
