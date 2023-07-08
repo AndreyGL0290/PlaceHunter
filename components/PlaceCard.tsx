8b403d19f2f712d23e8f57b160456d13768cbafd
AndreyGL0290 <andreygl2014@gmail.com>
2023-07-08 12:46:31 -0400

    Changed styles and added icons in Place component

--- components/PlaceCard.tsx
+++ components/PlaceCard.tsx
@@ -1,15 +1,22 @@
-import { place } from "../types/global"
+import { placeOSM } from "../types/global"
 
-export function Place ({ place }: { place: place}){
+export function Place ({ place }: { place: placeOSM }){
     return (
-        <ul className="flex flex-col items-center border-slate-700 border-solid border-2 rounded-xl bg-slate-200">
-            <img className="w-20 hover" src={place.icon} />
-            <p className="font-mono text-slate-900">{place.display_name}</p>
-        </ul>    
+        <div className="h-80 flex flex-row justify-evenly rounded-xl bg-beige-100 ">
+            <div className="flex justify-center items-center">
+                <div className="h-5/6 w-36 flex justify-center items-center bg-martinique-950">
+                    <img src={place.icon} alt={"Image of\n" + place.type} className="h-40 w-40 "/>
+                </div>
+            </div>
+            <div className="h-full w-3/4 flex flex-col justify-evenly items-center text-justify mx-5">
+                <p className="font-mono text-slate-900">{place.display_name}</p>
+                <button className="px-20 py-2 rounded-lg bg-beige-200 transition ease-in-out hover:bg-beige-300 hover:scale-105">More</button>
+            </div>
+        </div>    
     )
 }
 
-export function PlacesList({ places }: { places: place[]}) {
+export function PlacesList({ places }: { places: placeOSM[] }) {
     return (
         <>
             {places.map(place => (
