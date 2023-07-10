4b647011cef354d47e97d2482ad9d29f63f58695
AndreyGL0290 <andreygl2014@gmail.com>
2023-07-10 14:44:57 -0400

    Deleted

--- components/SearchBar.tsx
+++
@@ -1,8 +0,0 @@
-export default function SearchBar() {
-    return (
-        <form>
-            <input placeholder="What are You looking for?" className="rounded-lg"/>
-            <button className="">Search</button>
-        </form>
-    )
-}
