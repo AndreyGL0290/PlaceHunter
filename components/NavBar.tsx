ffcb9bbd2f6e637e22d94589536536561617a635
AndreyGL0290 <andreygl2014@gmail.com>
2023-07-12 13:31:54 -0400

    Added styled login and logut buttons

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -5,20 +5,20 @@ export default function NavBar() {
     let router = useRouter();
     return (
         <header>
-            <div className="h-28 grid grid-cols-3 text-center bg-black text-white text-2xl">
+            <div className="h-28 grid grid-cols-3 text-center bg-black text-white text-2xl [&_a]:navbar-button">
                 <h1 className="font-bold text-xl text-white absolute left-2 top-0 phone:left-auto">Place Hunter</h1>
                 <div className="navbar-div">
-                    <a href='/about' className="transition ease-in-out hover:scale-125">About</a>
+                    <a href='/about'>About</a>
                 </div>
                 <div className="navbar-div">
                     {
                     router.route.replace('/', '') == '' ?
-                    <a href='/search' className="transition ease-in-out hover:scale-125">Search</a> :
-                    <a href='/' className="text-center transition ease-in-out hover:scale-125">Home</a>
+                    <a href='/search' className="">Search</a> :
+                    <a href='/' className="">Home</a>
                     }
                 </div>
                 <div className="navbar-div">
-                    <a href="#footer" className="transition ease-in-out hover:scale-125">Contact Us</a>
+                    <a href="#footer" className="">Contact Us</a>
                 </div>
                 <ProfileWidget />
             </div>
--- components/ProfileWidget.tsx
+++ components/ProfileWidget.tsx
@@ -6,13 +6,16 @@ export default function ProfileWidget() {
     const router = useRouter()
     return (
         <>
-            {(user && router.route.replace('/', '') != 'profile') && 
-            <div className="w-72 absolute right-2 cursor-pointer" onClick={(e) => {location.href='/profile'}}>
+            {router.route.replace('/', '') != 'profile' ? (user ?
+            <div className="absolute right-2 cursor-pointer [&_span]:hover:scale-105" onClick={(e) => {location.href='/profile'}}>
                 <div className="flex justify-end">
-                    <span className="text-xl mr-5">{user.name}</span>
+                    <span className="text-xl mr-5 navbar-button">{user.name}</span>
                     <img src={user.picture} className="rounded-[50%] w-10 h-10"/>
                 </div>
-            </div>}
+            </div> :
+            <a href="api/auth/login" className="absolute right-2 text-xl">Login</a>) :
+            <a href="api/auth/logout" className="absolute right-2 text-xl">Logout</a>
+            }
         </>
     )    
 }
