878ca5024ab303e2dd732b273bd054db05d4b5c6
AndreyGL0290 <andreygl2014@gmail.com>
2023-07-15 11:41:48 -0400

    Added select logic

--- public/js/about.js
+++ public/js/about.js
@@ -1,21 +1,32 @@
 let select = document.getElementsByTagName('select')[0]
 let root = document.getElementById('tech-stack')
 
-let stack1 = {'JS Frameworks and extnsion': ['Express.JS', 'TypScript'], 'Template Language': ['Embedded JS'], 'Style': ['TailwindCSS'], 'API\'s': ['Auth0 API', 'OSM API', 'Geolocation API'], 'Database': ['MongoDB']}
-let stack2 = {'JS Frameworks and extnsion': ['Next.JS', 'React', 'TypScript'], 'Style': ['TailwindCSS'], 'API\'s': ['Auth0 API', 'OSM API', 'Geolocation API'], 'Database': ['MongoDB']}
+let stack1 = {'JS Frameworks and Extnsion': ['Express.JS', 'TypeScript'], 'Template Language': ['Embedded JS'], 'Style': ['TailwindCSS'], 'API\'s': ['Auth0 API', 'OSM API', 'Geolocation API'], 'Database': ['MongoDB']}
+let stack2 = {'JS Frameworks and Extnsion': ['Next.JS', 'React', 'TypeScript'], 'Style': ['TailwindCSS'], 'API\'s': ['Auth0 API', 'OSM API', 'Geolocation API'], 'Database': ['MongoDB']}
+console.log(stack1)
 
 select.addEventListener('change', e => {
-    if (e.currentTarget.value == 1) {
-        // Changing DOM for express
-    } else {
-        // Changing DOM for Next.JS
-    }
+    console.log(e.currentTarget.value)
+    e.currentTarget.value == 1 ? buildStack(stack1) : buildStack(stack2);
 })
 
-const firstStack = () => {
+const buildStack = (stack) => {
+    root.innerHTML = '';
+    for (const [key ,value] of Object.entries(stack)) {
+        let div = document.createElement('div');
+        
+        let h = document.createElement('h1');
+        h.className = 'techstack-header';
+        h.textContent = key;
+        div.appendChild(h);
 
-}
+        for (let item of value) {
+            let p = document.createElement('p');
+            p.className = 'techstack-item';
+            p.textContent = item;
+            div.appendChild(p);
+        }
 
-const secondStack = () => {
-    
+        root.appendChild(div);
+    }
 }
