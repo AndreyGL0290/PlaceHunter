236c3a7e28667e6c6c60707328f9f3233325e901
AndreyGL0290 <andreygl2014@gmail.com>
2023-07-18 13:30:10 -0400

    Represents tech stack list in Next.JS version

---
+++ components/TechStack/NextStack.tsx
@@ -0,0 +1,28 @@
+export default function NextStack() {
+    return (
+        <>
+            <div>
+                <h1 className="techstack-header">JS Frameworks and Extensions</h1>
+                <p className="techstack-item">Next.JS</p>
+                <p className="techstack-item">TypeScript</p>
+            </div>
+
+            <div>
+                <h1 className="techstack-header">Style</h1>
+                <p className="techstack-item">TailwindCSS</p>
+            </div>
+            
+            <div>
+                <h1 className="techstack-header">API's</h1>
+                <p className="techstack-item">Auth0 API</p>
+                <p className="techstack-item">OSM API</p>
+                <p className="techstack-item">Geolocation API</p>
+            </div>
+            
+            <div>
+                <h1 className="techstack-header">Database</h1>
+                <p className="techstack-item">MongoDB</p>
+            </div>
+        </>
+    )
+}
