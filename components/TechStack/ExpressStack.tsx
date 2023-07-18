085fc48653b80bbb1243e5f70d5689f51fb5d448
AndreyGL0290 <andreygl2014@gmail.com>
2023-07-18 13:29:45 -0400

    Represents tech stack list in Express.JS version

---
+++ components/TechStack/ExpressStack.tsx
@@ -0,0 +1,33 @@
+export default function ExpressStack() {
+    return (
+        <>
+            <div>
+                <h1 className="techstack-header">JS Frameworks and Extensions</h1>
+                <p className="techstack-item">Express.JS</p>
+                <p className="techstack-item">TypeScript</p>
+            </div>
+            <div>
+                <h1 className="techstack-header">Template Language</h1>
+                <p className="techstack-item">Embedded JS</p>
+            </div>
+
+            <div>
+                <h1 className="techstack-header">Style</h1>
+                <p className="techstack-item">TailwindCSS</p>
+            </div>
+            
+            <div>
+                <h1 className="techstack-header">API's</h1>
+                <p className="techstack-item">GraphQL</p>
+                <p className="techstack-item">Auth0 API</p>
+                <p className="techstack-item">OSM API</p>
+                <p className="techstack-item">Geolocation API</p>
+            </div>
+            
+            <div>
+                <h1 className="techstack-header">Database</h1>
+                <p className="techstack-item">MongoDB</p>
+            </div>
+        </>
+    )
+}
