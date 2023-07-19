a5ac97b87140f90c95d28e7d08f0461ca630648a
AndreyGL0290 <andreygl2014@gmail.com>
2023-07-19 17:30:52 -0400

    Rendering statistics

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,16 +1,41 @@
-import { useUser } from "@auth0/nextjs-auth0/client"
+import { useUser } from "@auth0/nextjs-auth0/client";
+import { getSession } from "@auth0/nextjs-auth0";
 
-export default function Profile() {
+import getMostSearchedUp from "../../utils/getMostSearchedUp";
+import mongoConnect from "../../mongoDB/connect";
+
+export default function Profile({ statistics }) {
     const { user } = useUser();
+    let mostSearchedUp = getMostSearchedUp(statistics)
     return (
         user &&
         <div className="flex flex-col justify-center items-center">
-            <h1>Your account data</h1>
-            <div className="text-center h-72 flex flex-col justify-center w-96">
-                <img src={user.picture} alt='Profile photo'/>
-                <h1>{user.name}</h1>
-                <p>{user.email}</p>
+            <div className="my-16 text-center flex flex-col justify-center items-center">
+                <h1 className="mb-10 text-2xl">Account data</h1>
+                <div className="flex flex-col justify-center items-center">
+                    <div className="flex flex-row justify-start items-center">
+                        <img src={user.picture} alt='Profile photo' className="h-16 w-16"/>
+                        <h1>{user.name}</h1>
+                    </div>
+                    <p>Email: {user.email}</p>
+                </div>
+            </div>
+            <div className="flex flex-col justify-center items-center">
+                <h1 className="mb-10 text-2xl">Statistics</h1>
+                <p>Most searched up: <b>{mostSearchedUp}: {statistics[mostSearchedUp]}</b></p>
             </div>
         </div>
     )
+}
+
+export async function getServerSideProps({req, res}){
+    const collection = await mongoConnect(process.env.DB, 'User')
+    const session = await getSession(req, res);
+    
+    let statistics = (await collection.findOne({ userId: session.user.sub })).queries
+    if (statistics === undefined) statistics = {};
+
+    return {
+        props: {statistics: statistics}
+    }
 }
