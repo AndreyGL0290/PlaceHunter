091e4f97fa26de7f89e8efe40ef1e6fc1cead79c
AndreyGL0290 <andreygl2014@gmail.com>
2023-07-20 01:29:51 -0400

    Changed Logout href

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,5 +1,5 @@
 // Changes Profile Widget on Logout button
 let profileWidget = document.getElementById('profile-widget')
 profileWidget.innerHTML = 'Logout'
-profileWidget.className = 'navbar-button'
-profileWidget.onclick = (e) => location.href='/'
+profileWidget.className = 'navbar-button cursor-pointer'
+profileWidget.addEventListener('click', (e) => {e.currentTarget.parentNode.onclick=(e) => {location.href='/logout'}})
